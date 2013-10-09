bc83fe7111ace9c230a450c0eb35ba7c444e4bc8
Rey Necesito <reynecesito@gmail.com>
2013-10-09 11:34:05 +0800

    added http.call for youtube connection

--- server/config.js
+++ server/config.js
@@ -7,3 +7,17 @@ Accounts.loginServiceConfiguration.insert({
 	clientId: "202055712427.apps.googleusercontent.com",
 	secret: "Wc9ELISnDLNZ-BBGTVbKUJ7w"
 });
+
+Meteor.methods({
+    checkYT: function () {
+        this.unblock();
+        return Meteor.http.call("GET", "https://www.googleapis.com/youtube/v3/channels?part=contentDetails%2Cstatistics%2Csnippet&forUsername=DevDiaryOnanyTV&key=AIzaSyDL6F2UDnezIht4VT-nnKpD_vZSu1ujEyY");
+    },
+    test_fn: function(params) {
+    	Meteor.http.get(url, {params: params}, function (err, result) {
+	        console.log(result.statusCode, result.data);
+	        var retdata =  result.data;
+    		Session.set("myItems", retdata.items);
+      	});
+    }
+});
